76be0f3f4caa1773e178b9cb6aea54993eec2d57
FreemanUA <84132305+FreemanUAaa@users.noreply.github.com>
2022-06-05 19:27:37 +0300

    Users service: create database interface

---
+++ NewsBlog/Services/Users/NewsBlog.Users.Core/Database/IDatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsBlog.Users.Core.Models;
+
+namespace NewsBlog.Users.Core.Database
+{
+    public interface IDatabaseContext
+    {
+        DbSet<User> Users { get; set; }
+
+        DbSet<Cover> Covers { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
