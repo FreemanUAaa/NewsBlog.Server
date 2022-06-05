12fbba3cac775086fdc91eda5f8e1da12f11bb55
FreemanUA <84132305+FreemanUAaa@users.noreply.github.com>
2022-06-05 20:23:00 +0300

    Users service: create exception strings class
    It contains error lines that are used when you want to cause an error.

---
+++ NewsBlog/Services/Users/NewsBlog.Users.Core/Exceptions/ExceptionStrings.cs
@@ -0,0 +1,19 @@
+﻿namespace NewsBlog.Users.Core.Exceptions
+{
+    public static class ExceptionStrings
+    {
+        public static string NotFound => "Path not found";
+
+        public static string UserNotFound => "User does not exist";
+
+        public static string EmailIsBusy => "This email is already used";
+
+        public static string EntityAlreadyExists => "entity already exists";
+
+        public static string ExtensionNotSupported => "extension not supported";
+
+        public static string UnexpectedErrorOccurred => "An unexpected error occurred";
+
+        public static string FailedToLogin => "The password or the email are no correct";
+    }
+}
